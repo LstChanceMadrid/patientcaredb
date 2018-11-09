64d7d6000c6b6c0175b1e1549d9fb2977f7d26f3
LstChanceMadrid <michael.madrid@live.com>
2018-11-09 14:22:55 -0600

    adding heroku

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ const db = pgp(connectionString);
 const session = require('express-session');
 const sess = {secret: 'keyboard cat', resave: false, saveUninitialized: false};
 const models = require('./models');
+const port = process.env.PORT || 3000;
+
+
+
+
+const DATABASE_URL = "postgresql-polished-34740"
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
+
 
 // access to main.css file
 app.use('/styles', express.static('styles'));
@@ -38,6 +64,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, (req, res) => {
+app.listen(port, (req, res) => {
     console.log('Server running...');
 });
