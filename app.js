8d9bd609f2b40f5a34a48c841b1d987d27712f5b
LstChanceMadrid <michael.madrid@live.com>
2018-11-11 16:09:59 -0600

    fixed employee redirecting error

--- app.js
+++ app.js
@@ -9,12 +9,8 @@ const session = require('express-session');
 const sess = {secret: 'keyboard cat', resave: false, saveUninitialized: false};
 const port = 3000;
 
-
-
-
-
-
 // access to main.css file
+
 app.use('/styles', express.static('styles'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +20,7 @@ app.engine('mustache', mustacheExpress());
 
 app.set('views', "./views"); 
 app.set('view engine', 'mustache');
+
 
 // hospital login page
 
@@ -41,29 +38,20 @@ app.post('/register-hospital', (req, res) => {
     let zipcode = req.body.zipcode;
     let telephone = req.body.telephone;
     
-    db.none('SELECT hospitalname, hospitalid FROM hospitals WHERE hospitalname = $1', [hospitalname])
-    .then(() => {
-            db.none('INSERT INTO hospitals(hospitalname, accesscode, address, city, state, zipcode, telephone) VALUES($1, $2, $3, $4, $5, $6, $7)', [hospitalname, accesscode, address, city, state, zipcode, telephone]).then(() => {
-                db.one('SELECT hospitalname, hospitalid FROM hospitals WHERE hospitalname = $1', [hospitalname]).then(hospital => {
-                    let hospitalid = hospital.hospitalid;
-                    console.log(hospitalname)
-                    res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
-                });
-            });
-    }).catch(e => {
-        let errorType = e.name;
-
-        if (errorType === 'QueryResultError') {
-            let hospitalExistsTrue = "That hospital is already registered";
-
-            res.render('./index', {hospitalExistsTrue : hospitalExistsTrue});
-        } else {
-            db.none('INSERT INTO hospitals(hospitalname, accesscode, address, city, state, zipcode, telephone) VALUES($1, $2, $3, $4, $5, $6, $7)', [hospitalname, accesscode, address, city, state, zipcode, telephone]).then(() => {
-                console.log(hospitalname)
+    db.none('SELECT hospitalname, hospitalid FROM hospitals WHERE hospitalname = $1', [hospitalname]).then(() => {
+        db.none('INSERT INTO hospitals(hospitalname, accesscode, address, city, state, zipcode, telephone) VALUES($1, $2, $3, $4, $5, $6, $7)', [hospitalname, accesscode, address, city, state, zipcode, telephone]).then(() => {
+            db.one('SELECT hospitalname, hospitalid FROM hospitals WHERE hospitalname = $1', [hospitalname]).then(hospital => {
+                let hospitalid = hospital.hospitalid;
                 res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
             });
+        });
+    }).catch(e => {
+        let errorType = e.name;
+        if (errorType === 'QueryResultError') {
+            res.redirect('..');
+        } else {
+            console.log(e);
         }
-        console.log(e);
     });
 });
 
@@ -73,18 +61,15 @@ app.post('/log-in-hospital', (req, res) => {
     let accesscode = req.body.accesscode;
 
     db.one('SELECT hospitalid, hospitalname FROM hospitals WHERE hospitalname = $1 AND accesscode = $2', [hospitalname, accesscode]).then(hospital => {
-        if (hospital) {
-            let hospitalname = hospital.hospitalname;
-            let hospitalid = hospital.hospitalid;
-            res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
-        } 
+        let hospitalname = hospital.hospitalname;
+        let hospitalid = hospital.hospitalid;
+
+        res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
     }).catch(e => {
         let errorType = e.name;
 
         if ( errorType === 'QueryResultError') {
-
-            let hospitalExistsFalse = "This is not a registered hospital";
-            res.render('./index', {hospitalExistsFalse : hospitalExistsFalse});
+            res.redirect('..');
         } else {
             console.log(e);
         }
@@ -92,23 +77,21 @@ app.post('/log-in-hospital', (req, res) => {
 });
 
 
-
 // employee login page
+
 app.get('/:hospitalname/:hospitalid/home', (req, res) => {
     let hospitalid = req.params.hospitalid;
 
     db.one('SELECT hospitalname, address, city, state, zipcode, telephone, hospitalid FROM hospitals WHERE hospitalid = $1', [hospitalid]).then(hospital => {
-        console.log(hospital)
+
         res.render('hospital-home', {hospital: hospital});
     });
 });
 
 app.post('/:hospitalname/:hospitalid/register-employee', (req, res) => {
 
-    let hospitalname = req.params.hospitalname;
     let hospitalid = req.params.hospitalid;
 
-    console.log(hospitalid)
     let username = req.body.username;
     let password = req.body.password;
     let employeefirstname = req.body.employeefirstname;
@@ -119,8 +102,8 @@ app.post('/:hospitalname/:hospitalid/register-employee', (req, res) => {
     let zipcode = req.body.zipcode;
     let telephone = req.body.telephone;
     
-    db.none('SELECT username, employeeid FROM employees WHERE username = $1', [username])
-    .then(() => {
+    db.none('SELECT username, employeeid FROM employees WHERE username = $1', [username]).then(() => {
+
         db.none('INSERT INTO employees(username, password, employeefirstname, employeelastname, address, city, state, zipcode, telephone, hospitalid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [username, password, employeefirstname, employeelastname, address, city, state, zipcode, telephone, hospitalid]).then(() => {
 
             db.one('SELECT employees.username, employees.employeeid, hospitals.hospitalname, hospitals.hospitalid FROM employees INNER JOIN hospitals ON employees.hospitalid = hospitals.hospitalid WHERE username = $1', [username]).then(result => {
@@ -131,9 +114,6 @@ app.post('/:hospitalname/:hospitalid/register-employee', (req, res) => {
                 let username = result.username;
                 let employeeid = result.employeeid;
 
-                console.log(username)
-                console.log(hospitalname)
-
                 res.redirect('/' + hospitalname + '/' + hospitalid + '/' + username + '/' + employeeid + '/home');
             });
         });
@@ -141,47 +121,54 @@ app.post('/:hospitalname/:hospitalid/register-employee', (req, res) => {
         let errorType = e.name;
 
         if (errorType === 'QueryResultError') {
-            let employeeExistsTrue = "That username is already registered";
+            db.one('SELECT hospitalname, address, city, state, zipcode, telephone, hospitalid FROM hospitals WHERE hospitalid = $1', [hospitalid]).then(hospital => {
 
-            res.render('hospital-home', {employeeExistsTrue : employeeExistsTrue});
-        } else {
-            db.none('INSERT INTO employees(username, password, employeefirstname, employeelastname, address, city, state, zipcode, telephone, hospitalid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [username, password, employeefirstname, employeelastname, address, city, state, zipcode, telephone, hospitalid]).then(() => {
-                console.log(hospitalname)
+                let hospitalname = hospital.hospitalname;
+                let hospitalid = hospital.hospitalid;
                 res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
             });
+        } else {
+            console.log(e);
         }
-        console.log(e);
     });
 });
 
-app.post('/log-in-hospital', (req, res) => {
+app.post('/:hospitalname/:hospitalid/log-in-employee', (req, res) => {
+    let hospitalname = req.params.hospitalname;
+    
+    let username = req.body.username;
+    let password = req.body.password;
 
-    let hospitalname = req.body.hospitalname;
-    let accesscode = req.body.accesscode;
+    db.one('SELECT employees.employeeid, employees.username, employees.password, hospitals.hospitalname, hospitals.hospitalid FROM employees INNER JOIN hospitals ON employees.hospitalid = hospitals.hospitalid WHERE employees.username = $1 AND employees.password = $2', [username, password]).then(result => {
 
-    db.one('SELECT hospitalid, hospitalname, accesscode FROM hospitals WHERE hospitalname = $1 AND accesscode = $2', [hospitalname, accesscode]).then(hospital => {
-        if (hospital) {
-            console.log(hospital)
-            console.log(hospital.hospitalname)
-            console.log(hospital.hospitalid)
-            let hospitalname = hospital.hospitalname;
-            let hospitalid = hospital.hospitalid;
-            console.log(hospital.hospitalid);
-            res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
-        } else {
-            let hospitalExistsFalse = "This is not a registered hospital";
-            console.log("its broken")
-            res.render('./index', {hospitalExistsFalse : hospitalExistsFalse});
-        }
+        let hospitalname = result.hospitalname;
+        let hospitalid = result.hospitalid;
+
+        let username = result.username;
+        let employeeid = result.employeeid;
+
+        res.redirect('/' + hospitalname + '/' + hospitalid + '/' + username + '/' + employeeid + '/home');
+        
     }).catch(e => {
-        console.log(e);
+        let errorType = e.name;
+
+        if (errorType === 'QueryResultError') {
+            db.one('SELECT hospitalid, hospitalname, address, city, state, zipcode, telephone FROM hospitals WHERE hospitalname = $1', [hospitalname]).then(hospital => {
+
+                let hospitalname = hospital.hospitalname
+                let hospitalid = hospital.hospitalid
+                res.redirect('/' + hospitalname + '/' + hospitalid + '/home');
+            });
+        } else {
+            console.log(e);
+        }
     });
 });
 
 
 app.get('/:hospital/:hospitalid/:username/:employeeid/home',(req, res) => {
     res.render('employee-home');
-})
+});
 
 
 
